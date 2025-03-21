ebb61466167a86767407165cf52f3c45da364c1a
ZhuoZheng <zhengzhuo_git@163.com>
2025-03-20 17:09:04 -0700

    separate eval function

--- torchange/metrics/bcd.py
+++ torchange/metrics/bcd.py
@@ -6,10 +6,45 @@
 
 import os.path
 
+import gc
 import torch
 import ever as er
 import numpy as np
 from tqdm import tqdm
+from torchange.metrics.function import binary_change_detection_evaluate
+
+
+@torch.no_grad()
+def binary_change_detection_evaluate(model, dataloader, log_dir=None, logger=None, class_names=None):
+    model.eval()
+    pm = er.metric.PixelMetric(2, log_dir, logger=logger, class_names=class_names)
+
+    for img, gt in tqdm(dataloader, disable=not er.dist.is_main_process()):
+        img = img.to(er.auto_device())
+        predictions = self.model(img)
+
+        pr_change = (predictions['change_prediction'] > 0.5).cpu()
+        pr_change = pr_change.numpy().astype(np.uint8)
+        gt_change = gt['masks'][-1]
+        gt_change = gt_change.numpy()
+        y_true = gt_change.ravel()
+        y_pred = pr_change.ravel()
+
+        y_true = np.where(y_true > 0, np.ones_like(y_true), np.zeros_like(y_true))
+
+        pm.forward(y_true, y_pred)
+
+    results = pm.summary_all()
+
+    torch.cuda.empty_cache()
+    gc.collect()
+
+    return {
+        'eval/iou': results.iou(1),
+        'eval/f1': results.f1(1),
+        'eval/prec': results.precision(1),
+        'eval/rec': results.recall(1),
+    }
 
 
 @er.registry.CALLBACK.register()
@@ -29,7 +64,8 @@ class BinaryChangeDetectionPixelEval(er.Callback):
         self.score_table_name = data_cfg.type
 
     def func(self):
-        score = self.evaluate()
+        score = binary_change_detection_evaluate(self.model, self.dataloader, self.model_dir, self.logger)
+
         best_score = self.score_tracker.highest_score('eval/f1')
         if score['eval/f1'] > best_score['eval/f1']:
             self.save_model('model-best.pth')
@@ -39,33 +75,3 @@ class BinaryChangeDetectionPixelEval(er.Callback):
 
         best_score = self.score_tracker.highest_score('eval/f1')
         self.launcher.logger.info(f"best F1: {best_score['eval/f1']}, at step {best_score['step']}")
-
-    @torch.no_grad()
-    def evaluate(self):
-        self.model.eval()
-        pm = er.metric.PixelMetric(2, self.model_dir, logger=self.logger)
-
-        for img, gt in tqdm(self.dataloader, disable=not er.dist.is_main_process()):
-            img = img.to(er.auto_device())
-            predictions = self.model(img)
-
-            pr_change = (predictions['change_prediction'] > 0.5).cpu()
-            pr_change = pr_change.numpy().astype(np.uint8)
-            gt_change = gt['masks'][-1]
-            gt_change = gt_change.numpy()
-            y_true = gt_change.ravel()
-            y_pred = pr_change.ravel()
-
-            y_true = np.where(y_true > 0, np.ones_like(y_true), np.zeros_like(y_true))
-
-            pm.forward(y_true, y_pred)
-
-        results = pm.summary_all()
-
-        torch.cuda.empty_cache()
-        return {
-            'eval/iou': results.iou(1),
-            'eval/f1': results.f1(1),
-            'eval/prec': results.precision(1),
-            'eval/rec': results.recall(1),
-        }
