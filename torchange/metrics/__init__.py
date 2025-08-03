08896a2d7ca32962f0792a63ec8d4fd27292f1d1
Zhuo Zheng <30289526+Z-Zheng@users.noreply.github.com>
2025-08-02 22:05:17 -0700

    feat: add unified evaluate interface

--- torchange/metrics/__init__.py
+++ torchange/metrics/__init__.py
@@ -1,5 +1,37 @@
 # Copyright (c) Zhuo Zheng and affiliates.
 # All rights reserved.
-
 # This source code is licensed under the license found in the
 # LICENSE file in the root directory of this source tree.
+
+from typing import Callable, Dict
+
+from .bcd import binary_change_detection_evaluate
+from .second import semantic_change_detection_evaluate
+from .xview2 import evaluate as xview2_evaluate
+
+_EVAL_REGISTRY: Dict[str, Callable] = {
+    'bcd': binary_change_detection_evaluate,
+    'second': semantic_change_detection_evaluate,
+    'xview2': xview2_evaluate,
+}
+
+
+def evaluate(task: str, model, dataloader, *args, **kwargs):
+    """Unified evaluation interface across different change detection tasks.
+
+    Parameters
+    ----------
+    task: str
+        Name of the evaluation task. Options are ``'bcd'``, ``'second'``, and
+        ``'xview2'``.
+    model: torch.nn.Module
+        Model to be evaluated.
+    dataloader: Iterable
+        Dataloader providing evaluation data.
+    *args, **kwargs:
+        Additional arguments forwarded to the underlying evaluation function.
+    """
+    if task not in _EVAL_REGISTRY:
+        raise KeyError(f"Unknown evaluation task: {task}. Supported tasks: {list(_EVAL_REGISTRY.keys())}")
+    return _EVAL_REGISTRY[task](model, dataloader, *args, **kwargs)
+
