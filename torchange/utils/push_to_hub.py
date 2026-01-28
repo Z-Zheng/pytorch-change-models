c00b3c30042a51a94cc4c97871d9fa5013f4de01
ZhuoZheng <zhengzhuo_git@163.com>
2026-01-27 20:45:13 -0800

    add `push_to_hub.py` utility for integrating model uploads to Hugging Face Hub

---
+++ torchange/utils/push_to_hub.py
@@ -0,0 +1,46 @@
+# Copyright (c) Zhuo Zheng and affiliates.
+# All rights reserved.
+
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+import ever as er
+import fire
+from huggingface_hub import ModelCard, ModelCardData
+
+DEFAULT_MODEL_CARD = """
+---
+# For reference on model card metadata, see the spec: https://github.com/huggingface/hub-docs/blob/main/modelcard.md?plain=1
+# Doc / guide: https://huggingface.co/docs/hub/model-cards
+{{ card_data }}
+---
+
+- Model class: {{ model_name }}
+- Source: {{ model_source }}
+- Codebase: {{ library_name }}
+"""
+
+
+def model_dir_to_hub(model_dir, repo_id, private=False, checkpoint_name=None, model_card: ModelCard = None):
+    er.registry.register_modules()
+    model, _ = er.infer_tool.build_from_model_dir(model_dir=model_dir, checkpoint_name=checkpoint_name)
+
+    full_name = f"{model.__module__}.{model.__class__.__name__}"
+    model._hub_mixin_info.model_card_data.library_name = 'torchange'
+    model._hub_mixin_info.model_card_data.model_name = model.__class__.__name__
+    model._hub_mixin_info.model_card_data.model_source = full_name
+    model._hub_mixin_info.model_card_template = DEFAULT_MODEL_CARD
+
+    model: er.ERModule
+    model.push_to_hub(repo_id=repo_id, private=private)
+
+    if model_card is not None:
+        model_card.data.library_name = 'torchange'
+        model_card.data.model_name = model.__class__.__name__
+        model_card.data.model_source = full_name
+        model_card.push_to_hub(repo_id, repo_type='model')
+
+
+if __name__ == '__main__':
+    fire.Fire({
+        'model_dir_to_hub': model_dir_to_hub
+    })
