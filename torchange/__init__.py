673bce62f6725f1fdaf1ba4ddfa36c8ce6771a98
ZhuoZheng <zhengzhuo_git@163.com>
2026-01-31 14:49:45 -0800

    bump to 0.0.4

--- torchange/__init__.py
+++ torchange/__init__.py
@@ -3,7 +3,7 @@
 
 # This source code is licensed under the license found in the
 # LICENSE file in the root directory of this source tree.
-__version__ = "0.0.3a"
+__version__ = "0.0.4"
 
 import importlib
 import pkgutil
